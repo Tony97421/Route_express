b60e4253b05beedbb4c41a1ab3559c1bb6f90966
Techer Tony <techer307@gmail.com>
2023-11-29 15:46:55 +0100

    Delete user

--- src/controllers/movieControllers.js
+++ src/controllers/movieControllers.js
@@ -96,6 +96,24 @@ const updateMovie = (req, res) => {
   });
 };
 
+const deleteMovie = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  database
+    .query("delete from movies where id = ?", [id])
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.sendStatus(404);
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
 
 
 
@@ -104,4 +122,5 @@ module.exports = {
   getMovieById,
   postMovie,
   updateMovie,
+  deleteMovie,
 };
